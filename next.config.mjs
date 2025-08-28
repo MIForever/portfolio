b658a2b85018798d7fe3d0aebb5f2fc9cb6f92d7
Inoyatullo Musayev <86780028+MIForever@users.noreply.github.com>
2025-08-28 16:04:32 +0500

    fixing deploy sutff

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export', // Enable static export for GitHub Pages
   trailingSlash: true, // Required for GitHub Pages
+  basePath: process.env.NODE_ENV === 'production' ? '/mif-portfolio' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
